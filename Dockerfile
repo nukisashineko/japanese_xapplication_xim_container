f836a65a222c781f366f8aefb46e4878f6e3a6f6
nukisashineko <nukisashineko@gmail.com>
2020-07-26 16:02:15 +0900

    [mod] run idea with non root user

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,30 @@ RUN apt update -y && apt upgrade -y
 RUN apt install -y --no-install-recommends apt-utils
 RUN apt install ubuntu-desktop -y
 
-RUN useradd -ms /bin/bash vagrant
-USER vagrant 
-WORKDIR /home/vagrant
-ENTRYPOINT []
+# add vagrant sudoer user
+RUN apt install sudo -y
+#RUN useradd -rm -d /home/vagrant -s /bin/bash -G sudo vagrant
+#USER vagrant
+#WORKDIR /home/vagrant
+#RUN pwconv
+#SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+#RUN echo 'vagrant:xpasswordx' | chpasswd 
+
+#ENTRYPOINT []
+# Use mirror repository
+RUN sed -i.bak -e "s%http://[^ ]\+%mirror://mirrors.ubuntu.com/mirrors.txt%g" /etc/apt/sources.list
+
+# Install prerequisite packages
+RUN apt update \
+ && apt -yq dist-upgrade \
+ && apt install -yq --no-install-recommends \
+      sudo \
+      supervisor \
+ && apt clean \
+ && rm -rf /var/lib/apt/lists/*
+
+# Boot process
+COPY mount_point/etc/supervisord.conf /etc
+COPY mount_point/var/tmp/docker-entrypoint.sh /var/tmp
+CMD bash -E /var/tmp/docker-entrypoint.sh
+
---
+++ mount_point/var/tmp/docker-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -Ceux
+
+assert_env_defined () {
+  if [ ! -v $1 ]; then
+    echo "please define '$1' environment variable"
+    exit 1
+  fi
+}
+
+: "Add non administrative user" && {
+
+  : "Add user" && {
+    assert_env_defined USER_NAME
+    set +e  # avoid error check temporarily
+    id ${USER_NAME}  # existence check
+    if [ $? -ne 0 ]; then
+      useradd -s /bin/bash -m ${USER_NAME}
+    fi
+    set -e  # enable error check
+  }
+
+  : "Set home directory" && {
+    export HOME=/home/${USER_NAME}
+  }
+
+  : "Change user id" && {
+    assert_env_defined USER_ID
+    if [ $(id -u ${USER_NAME}) -ne ${USER_ID} ]; then
+      usermod -u ${USER_ID} ${USER_NAME}
+    fi
+  }
+
+  : "Change group id" && {
+    assert_env_defined GROUP_ID
+    if [ $(id -g ${USER_NAME}) -ne ${GROUP_ID} ]; then
+      usermod -g ${GROUP_ID} ${USER_NAME}
+    fi
+  }
+
+  : "Add common groups" && {
+    usermod -aG sudo ${USER_NAME}
+  }
+
+}
+
+: "Start supervisord" && {
+  supervisord -c /etc/supervisord.conf
+}
