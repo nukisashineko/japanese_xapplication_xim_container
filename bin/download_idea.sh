9c9189b040760403c615488976651392f844d9bd
nukisashineko <nukisashineko@gmail.com>
2020-07-29 03:28:03 +0900

    [mod] fix donwload_idea.sh

--- bin/download_idea.sh
+++ bin/download_idea.sh
@@ -2,14 +2,14 @@
 
 # user setting
 IDE_DOWNLOAD_URL='https://download-cf.jetbrains.com/idea/ideaIU-2020.1.3.tar.gz'
-TAR_GZ_FILE='ideaIU-2020.1.3.tar.gz'
+TAR_GZ_FILE_PATH='ideaIU-2020.1.3.tar.gz'
 
 # download and extract
 IDE_FOLDER_PATH='mount_point/opt/idea/'
 cd ${IDE_FOLDER_PATH}
-if [ ! -e ${TAR_GZ_FILE_PATH} ] ; then
-  curl -o ${TAR_GZ_FILE_PATH} ${IDE_DOWNLOAD_URL}
-  tar zxvf ${TAR_GZ_FILE_PATH}
+if [ ! -f "${TAR_GZ_FILE_PATH}" ] ; then
+  curl -o "${TAR_GZ_FILE_PATH}" "${IDE_DOWNLOAD_URL}"
+  tar zxvf "${TAR_GZ_FILE_PATH}"
 fi
 
 
